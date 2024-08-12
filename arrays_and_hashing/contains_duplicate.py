bf04637d66aa2c447bce229ecea8e959661f3917
alexsts <astserba@gmail.com>
2024-08-12 18:40:22 +0100

    ValidAnagram & ContainsDuplicate solutions

---
+++ arrays_and_hashing/contains_duplicate.py
@@ -0,0 +1,22 @@
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+# Time Complexity: O(n), Space Complexity: O(n)
+from typing import List
+
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for num in nums:
+            if num in hashset:
+                return True
+            else:
+                hashset.add(num)
+
+        return False
+
+
+solution = Solution()
+print(solution.hasDuplicate([1, 2, 3, 3]))
+print(solution.hasDuplicate([1, 2, 3, 4]))
+
---
+++ arrays_and_hashing/valid_anagram.py
@@ -0,0 +1,20 @@
+# Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_count = {}
+        t_count = {}
+
+        for i in range(len(s)):
+            s_count[s[i]] = 1 + s_count.get(s[i], 0)
+            t_count[t[i]] = 1 + t_count.get(t[i], 0)
+
+        return s_count == t_count
+
+
+solution = Solution()
+print(solution.isAnagram("racecar", "carrace"))
+print(solution.isAnagram("jar", "jam"))
